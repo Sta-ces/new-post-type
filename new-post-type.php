ee46affeff307ea8841f7de91beee739dda66782
Cedric Staces <34098638+Sta-ces@users.noreply.github.com>
2026-07-08 17:18:28 +0200

    Text Domain updated

--- new-post-type.php
+++ new-post-type.php
@@ -6,7 +6,7 @@
  * Version: 3.3.1
  * Author: Cedric Staces
  * Author URI: https://staces.be/
- * Text Domain: stacesbuilder
+ * Text Domain: staces-builder-theme
  */
 
 if (!defined('ABSPATH')) { exit; }
